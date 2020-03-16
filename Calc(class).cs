4af7e7fb4abf0b551dc288840d9223e6efb5556e
DouglasRamosP <43833349+DouglasRamosP@users.noreply.github.com>
2020-03-15 22:52:16 -0300

    Add files via upload

---
+++ Calc(class).cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	public class Calculadora
+	{
+		private List<double> listNumero = new List<double>();
+
+		public void addNumero(double num)
+		{
+			this.listNumero.Add(num);
+		}
+
+		public double Soma()
+		{
+			double soma = 0;
+			foreach (double numero in this.listNumero)
+			{
+				soma += numero;
+			}
+			this.listNumero.Clear();
+			return soma;
+		}
+		public double Subtrai()
+		{
+			double diferenca = this.listNumero[0];
+			this.listNumero.RemoveAt(0);
+			foreach (double numero in this.listNumero)
+			{
+				diferenca -= numero;
+			}
+			this.listNumero.Clear();
+			return diferenca;
+		}
+
+		public double Multiplica()
+		{
+			double produto = this.listNumero[0];
+			this.listNumero.RemoveAt(0);
+			foreach (double numero in this.listNumero)
+			{
+				produto *=  numero;
+			}
+			this.listNumero.Clear();
+			return produto;
+		}
+		public double Dividi()
+		{
+			double quociente = this.listNumero[0];
+			this.listNumero.RemoveAt(0);
+			foreach (double numero in this.listNumero)
+			{
+				quociente /= numero;
+			}
+			this.listNumero.Clear();
+			return quociente;
+		}
+
+
+	}
+}
---
+++ PROGRAMA(main).cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+	class PROGRAMA
+	{
+		static void Main(string[] args)
+		{
+            Calculadora c = new Calculadora();
+            c.addNumero(10.1);
+            c.addNumero(2.9);
+            System.Console.WriteLine(c.soma());
+
+
+            c.addNumero(10.1);
+            c.addNumero(4.9);
+            System.Console.WriteLine(c.soma());
+
+            c.addNumero(10);
+            c.addNumero(5);
+            c.addNumero(2);
+            System.Console.WriteLine(c.subtrai());
+
+            c.addNumero(10);
+            c.addNumero(2);
+            c.addNumero(2);
+            System.Console.WriteLine(c.multiplica());
+
+            c.addNumero(11);
+            c.addNumero(2);
+            System.Console.WriteLine(c.dividi());
+
+            System.Console.ReadKey();
+        }
+	}
+}
